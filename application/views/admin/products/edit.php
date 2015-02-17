1de4c4cfd8d2b9fb9b5c9fd91a89499a4c3acbce
ankmobikasa <ankushmadaan@mobikasa.com>
2015-02-17 17:56:07 +0530

    view updated

---
+++ application/views/admin/products/edit.php
@@ -0,0 +1,93 @@
+    <div class="container top">
+      
+      <ul class="breadcrumb">
+        <li>
+          <a href="<?php echo site_url("admin"); ?>">
+            <?php echo ucfirst($this->uri->segment(1));?>
+          </a> 
+          <span class="divider">/</span>
+        </li>
+        <li>
+          <a href="<?php echo site_url("admin").'/'.$this->uri->segment(2); ?>">
+            <?php echo ucfirst($this->uri->segment(2));?>
+          </a> 
+          <span class="divider">/</span>
+        </li>
+        <li class="active">
+          <a href="#">Update</a>
+        </li>
+      </ul>
+      
+      <div class="page-header">
+        <h2>
+          Updating <?php echo ucfirst($this->uri->segment(2));?>
+        </h2>
+      </div>
+
+ 
+      <?php
+      //flash messages
+      if($this->session->flashdata('flash_message')){
+        if($this->session->flashdata('flash_message') == 'updated')
+        {
+          echo '<div class="alert alert-success">';
+            echo '<a class="close" data-dismiss="alert">×</a>';
+            echo '<strong>Well done!</strong> product updated with success.';
+          echo '</div>';       
+        }else{
+          echo '<div class="alert alert-error">';
+            echo '<a class="close" data-dismiss="alert">×</a>';
+            echo '<strong>Oh snap!</strong> change a few things up and try submitting again.';
+          echo '</div>';          
+        }
+      }
+      ?>
+      
+      <?php
+      //form data
+      $attributes = array('class' => 'form-horizontal', 'id' => '');
+      $options_manufacture = array('' => "Select");
+      foreach ($manufactures as $row)
+      {
+        $options_manufacture[$row['id']] = $row['name'];
+      }
+
+      //form validation
+      echo validation_errors();
+
+      echo form_open('admin/products/update/'.$this->uri->segment(4).'', $attributes);
+      ?>
+        <fieldset>
+          <div class="control-group">
+            <label for="inputError" class="control-label">amazon_sku</label>
+            <div class="controls">
+              <input type="text" id="" name="amazon_sku" value="<?php echo $product[0]['amazon_sku']; ?>" >
+              <!--<span class="help-inline">Woohoo!</span>-->
+            </div>
+          </div>
+                  
+          <div class="control-group">
+            <label for="inputError" class="control-label">Cost Price</label>
+            <div class="controls">
+              <input type="text" id="" name="shopify_sku" value="<?php echo $product[0]['shopify_sku'];?>">
+              <!--<span class="help-inline">Cost Price</span>-->
+            </div>
+          </div>
+          <div class="control-group">
+            <label for="inputError" class="control-label">Sell Price</label>
+            <div class="controls">
+              <input type="text" name="product_id" value="<?php echo $product[0]['product_id']; ?>">
+              <!--<span class="help-inline">OOps</span>-->
+            </div>
+          </div>
+          
+          <div class="form-actions">
+            <button class="btn btn-primary" type="submit">Save changes</button>
+            <button class="btn" type="reset">Cancel</button>
+          </div>
+        </fieldset>
+
+      <?php echo form_close(); ?>
+
+    </div>
+     
---
+++ application/views/admin/products/list.php
@@ -0,0 +1,100 @@
+    <div class="container top">
+
+      <ul class="breadcrumb">
+        <li>
+          <a href="<?php echo site_url("admin"); ?>">
+            <?php echo ucfirst($this->uri->segment(1));?>
+          </a> 
+          <span class="divider">/</span>
+        </li>
+        <li class="active">
+          <?php echo ucfirst($this->uri->segment(2));?>
+        </li>
+      </ul>
+
+      <div class="page-header users-header">
+        <h2>
+          <?php echo ucfirst($this->uri->segment(2));?> 
+          <a  href="<?php echo site_url("admin").'/'.$this->uri->segment(2); ?>/add" class="btn btn-success">Add a new</a>
+        </h2>
+      </div>
+      
+      <div class="row">
+        <div class="span12 columns">
+          <div class="well">
+           
+            <?php
+           
+            $attributes = array('class' => 'form-inline reset-margin', 'id' => 'myform');
+           
+            $options_manufacture = array(0 => "all");
+            //foreach ($manufactures as $row)
+            //{
+            //  $options_manufacture[$row['id']] = $row['name'];
+           // }
+            //save the columns names in a array that we will use as filter         
+            $options_products = array();    
+            foreach ($products as $array) {
+              foreach ($array as $key => $value) {
+                $options_products[$key] = $key;
+              }
+              break;
+            }
+
+            echo form_open('admin/products', $attributes);
+     
+              echo form_label('Search Amazon Sku:', 'search_string');
+              echo form_input('search_string', $search_string_selected, 'style="width: 170px;
+height: 26px;"');
+
+              echo form_label('Filter:', 'manufacture_id');
+              echo form_dropdown('manufacture_id', $options_manufacture, $manufacture_selected, 'class="span2"');
+
+              echo form_label('Order by:', 'order');
+              echo form_dropdown('order', $options_products, $order, 'class="span2"');
+
+              $data_submit = array('name' => 'mysubmit', 'class' => 'btn btn-primary', 'value' => 'Go');
+
+              $options_order_type = array('Asc' => 'Asc', 'Desc' => 'Desc');
+              echo form_dropdown('order_type', $options_order_type, $order_type_selected, 'class="span1"');
+
+              echo form_submit($data_submit);
+
+            echo form_close();
+            ?>
+
+          </div>
+
+          <table class="table table-striped table-bordered table-condensed">
+            <thead>
+              <tr>
+                <th class="header">#</th>
+                <th class="yellow header headerSortDown">amazon_sku</th>
+                <th class="red header">shopify_sku</th>
+                <th class="red header">product_id</th>
+                <th class="red header">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+              foreach($products as $row)
+              {
+                echo '<tr>';
+                echo '<td>'.$row['id'].'</td>';
+                echo '<td>'.$row['amazon_sku'].'</td>';
+                echo '<td>'.$row['shopify_sku'].'</td>';
+                echo '<td>'.$row['product_id'].'</td>';
+                echo '<td class="crud-actions">
+                  <a href="'.site_url("admin").'/products/update/'.$row['id'].'" class="btn btn-info">view & edit</a>  
+                  <a href="'.site_url("admin").'/products/delete/'.$row['id'].'" class="btn btn-danger">delete</a>
+                </td>';
+                echo '</tr>';
+              }
+              ?>      
+            </tbody>
+          </table>
+
+          <?php echo '<div class="pagination">'.$this->pagination->create_links().'</div>'; ?>
+
+      </div>
+    </div>
